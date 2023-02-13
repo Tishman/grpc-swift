dfffac72bf0c4810a5d87e7981fd0798ac869c08
Роман Тищенко <romantisenko@Romans-MacBook-Pro.local>
2023-02-13 11:25:59 +0800

    Create framework

---
+++ grpc-swift/grpc-swift/grpc_swift.h
@@ -0,0 +1,18 @@
+//
+//  grpc_swift.h
+//  grpc-swift
+//
+//  Created by Роман Тищенко on 13.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for grpc_swift.
+FOUNDATION_EXPORT double grpc_swiftVersionNumber;
+
+//! Project version string for grpc_swift.
+FOUNDATION_EXPORT const unsigned char grpc_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <grpc_swift/PublicHeader.h>
+
+
